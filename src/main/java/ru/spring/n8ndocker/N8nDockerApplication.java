dd16e0c7758cedf502ce375eeb99e4418af78406
Nn17 <yakor356@gmail.com>
2024-11-15 23:11:41 +0300

    init commit

---
+++ src/main/java/ru/spring/n8ndocker/N8nDockerApplication.java
@@ -0,0 +1,13 @@
+package ru.spring.n8ndocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N8nDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N8nDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/spring/n8ndocker/N8nDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.n8ndocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N8nDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
